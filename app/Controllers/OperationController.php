7cc1468fc301086fc1bef93afb08c96d4cdd2ec2
Noumsmam <noumsrasolondraibe@gmail.com>
2026-07-20 12:03:13 +0300

    transfert

--- app/Controllers/OperationController.php
+++ app/Controllers/OperationController.php
@@ -28,18 +28,55 @@ class OperationController extends BaseController
         $client = new Client();
         $clientInf = $client->find($id);
         $offre = new Offre();
-        $idOffre = $offre->checkOffre($montant);
         $offreFrais = new Offre_frais();
+        $idOffre = $offre->checkOffre($montant);
         $frais = $offreFrais->getFrais($idOffre);
-        $solde = $clientInf['solde'] - $montant - $frais;
-        $data=['solde' => $solde];
-        $client->update($id,$data);
-        $historique = new Historique_transaction();
-        $dataHisto = [
-            'id_client' => $id,
-            'montant' => $montant,
-            'id_operation' => 2
-        ];
-        $historique->save($data);
+        if($client->checkSolde($id,$montant+$frais)){
+            $solde = $clientInf['solde'] - $montant - $frais;
+            $data=['solde' => $solde];
+            $client->update($id,$data);
+            $historique = new Historique_transaction();
+            $dataHisto = [
+                'id_client' => $id,
+                'montant' => $montant,
+                'id_operation' => 2
+            ];
+            $historique->save($dataHisto);
+        } else {
+            return view('home',['error' => 'fond insuffisant']);
+        }
+        return view('home');
+    }
+
+    function transfert($client2,$montant,$client1) {
+        $client = new Client();
+        $clientInf = $client->find($client1);
+        $offre = new Offre();
+        $offreFrais = new Offre_frais();
+        $idOffre = $offre->checkOffre($montant);
+        $frais = $offreFrais->getFrais($idOffre);
+        if($client->checkSolde($client1,$montant+$frais)) {
+            $solde1 = $clientInf['solde'] - $montant - $frais;
+            $data1 = ['solde' => $solde1];
+            $client->update($client1,$data1);
+            $historique = new Historique_transaction();
+            $dataHisto1 = [
+                'id_client' => $client1,
+                'montant' => $montant,
+                'id_operation' => 3
+            ];
+            $historique->save($dataHisto1);
+
+            $solde2 = $clientInf['solde'] + $montant;
+            $data2 = ['solde' => $solde2];
+            $client->update($client2,$data2);
+            $historique = new Historique_transaction();
+            $dataHisto2 = [
+                'id_client' => $client2,
+                'montant' => $montant,
+                'id_operation' => 1
+            ];
+            $historique->save($dataHisto2);
+        }
     }
 }
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -7,4 +7,15 @@ use CodeIgniter\Model;
         protected $table = 'Client';
         protected $primaryKey = 'id';
         protected $allowedFields = ['id_prefixe','numero','solde','nom'];
+
+        function checkSolde($id,$montant) {
+            $client = new Client();
+            $clientInf = $client->find($id);
+            $solde = $clientInf['solde'];
+            $check = true;
+            if($solde < $montant) {
+                $check = false;
+            }
+            return $check;
+        }
     }
